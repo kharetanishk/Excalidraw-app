db4a13a9e27653437887b16ea335a38232ff1bc4
Tanishk khare <kharetanishk18@gmail.com>
2025-09-18 18:07:51 +0530

    completed the http-layer

--- apps/http-backend/src/controllers/auth.controller.ts
+++ apps/http-backend/src/controllers/auth.controller.ts
@@ -64,3 +64,7 @@ export const signin = async (
     next(err);
   }
 };
+
+export const check = (req: Request, res: Response) => {
+  res.send("hello world");
+};
--- apps/http-backend/src/routes/auth.route.ts
+++ apps/http-backend/src/routes/auth.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { signup, signin } from "../controllers/auth.controller";
+import { signup, signin, check } from "../controllers/auth.controller";
 import { validateBody } from "../middlewares/validate";
+
 import { createUserSchema, signInSchema } from "@repo/common/types";
 
 const authroutes: Router = Router();
 
 authroutes.post("/signup", validateBody(createUserSchema), signup);
 authroutes.post("/signin", validateBody(signInSchema), signin);
+authroutes.get("/me", check);
 
 export default authroutes;
