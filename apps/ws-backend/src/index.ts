6ce50cdfeae1c4d1c2a29b4d843884a92eb9bfd3
Tanishk khare <kharetanishk18@gmail.com>
2025-09-21 18:01:10 +0530

    step - 1 Authenticate via token. Join a room. Leave a room. See events like user_joined / user_left.
    step -2
    Users can see who’s online/offline in their rooms.
    Users see typing… indicators.
    Users can send & receive messages in real-time.
    Server acknowledges each message (so frontend can show delivered).
    Spam control (basic).

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -15,3 +15,18 @@ const io = new Server(httpServer, {
 httpServer.listen(8000, () => {
   console.log(`WS server listening on port 8000 `);
 });
+
+//i have break the full functionality of ws-backend into steps--
+/* 
+step - 1 Authenticate via token.
+Join a room.
+Leave a room.
+See events like user_joined / user_left.
+
+step -2 
+Users can see who’s online/offline in their rooms.
+Users see “typing…” indicators.
+Users can send & receive messages in real-time.
+Server acknowledges each message (so frontend can show delivered).
+Spam control (basic).
+*/
